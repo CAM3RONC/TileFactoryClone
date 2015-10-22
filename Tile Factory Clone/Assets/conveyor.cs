abe76857fd0479cbacd959f10210453f485f6c4b
CAM3RON <camjchan@hotmail.com>
2015-10-22 18:05:24 +1100

    0.0.1
    Initialization

---
+++ Tile Factory Clone/Assets/conveyor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class conveyor : MonoBehaviour {
+
+	public Object conv;
+
+
+	void Start () {
+		conv = GameObject.Find("Conveyor");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
